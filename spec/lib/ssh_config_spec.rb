70785b1882b008a6273af3257a77827f73640d20
Christian Höltje <docwhat@gerf.org>
2017-04-08 22:41:42 -0400

    smoke test for defaults

--- spec/lib/ssh_config_spec.rb
+++ spec/lib/ssh_config_spec.rb
@@ -3,4 +3,10 @@ require 'ssh_config'
 
 RSpec.describe SshConfig do
   subject(:ssh_config) { described_class }
+
+  describe 'load_defaults' do
+    subject(:result) { described_class.load_defaults }
+
+    it { is_expected.to be_a Hash }
+  end
 end
